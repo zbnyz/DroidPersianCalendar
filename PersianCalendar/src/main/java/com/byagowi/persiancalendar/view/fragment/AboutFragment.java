320911153b41a350494fd8e0c41a5ff8fc635b73
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2018-06-17 02:25:00 +0430

    use ButterKnife in AboutFragment

--- PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/fragment/AboutFragment.java
+++ PersianCalendar/src/main/java/com/byagowi/persiancalendar/view/fragment/AboutFragment.java
@@ -15,12 +15,22 @@ import android.widget.TextView;
 import com.byagowi.persiancalendar.R;
 import com.byagowi.persiancalendar.util.Utils;
 
+import java.text.MessageFormat;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * About Calendar Activity
  *
  * @author ebraminio
  */
 public class AboutFragment extends Fragment {
+
+    @BindView(R.id.version2)
+    TextView versionTextView;
+    @BindView(R.id.license)
+    TextView licenseTextView;
 
     public String programVersion() {
         try {
@@ -35,18 +45,15 @@ public class AboutFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about, container, false);
+        ButterKnife.bind(this, view);
         Utils utils = Utils.getInstance(getContext());
         utils.setActivityTitleAndSubtitle(getActivity(), getString(R.string.about), "");
 
         String version = programVersion();
 
-        TextView versionTextView = view.findViewById(R.id.version2);
-        versionTextView.setText(getString(R.string.version) + " " +
-                utils.formatNumber(version.split("-")[0]));
+        versionTextView.setText(MessageFormat.format("{0} {1}", getString(R.string.version), utils.formatNumber(version.split("-")[0])));
 
-        TextView licenseTextView = view.findViewById(R.id.license);
-        licenseTextView.setText("Android Persian Calendar Version " + version + "\n" +
-                utils.readRawResource(R.raw.credits));
+        licenseTextView.setText(MessageFormat.format("Android Persian Calendar Version {0}\n{1}", version, utils.readRawResource(R.raw.credits)));
 
         Linkify.addLinks(licenseTextView, Linkify.WEB_URLS | Linkify.EMAIL_ADDRESSES);
 
